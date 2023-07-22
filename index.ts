36318a8c8c13d8886f2cee7c46274ceae3ebc3f8
Jason Raimondi <jason@raimondi.us>
2023-07-22 00:37:24 -0400

    feat(untested): add async browser storage class

--- index.ts
+++ index.ts
@@ -16,10 +16,24 @@ export type Adapter<SetConfig = unknown> = {
   setItem(key: string, value: string, config?: SetConfig): void;
 };
 
+export type AsyncAdapter<SetConfig = unknown> = {
+  clear?(): Promise<void>;
+  getItem(key: string): Promise<string | null>;
+  removeItem(key: string): Promise<void>;
+  setItem(key: string, value: string): Promise<void>;
+  setItem(key: string, value: string, config?: SetConfig): Promise<void>;
+};
+
 export type StorageConfig = {
   prefix?: string;
   serializer?: Serializer;
   adapter?: Adapter;
+};
+
+export type AsyncStorageConfig = {
+  prefix?: string;
+  serializer?: Serializer;
+  adapter?: AsyncAdapter;
 };
 
 export type DefineResponse<SetConfig = unknown> = {
@@ -28,12 +42,48 @@ export type DefineResponse<SetConfig = unknown> = {
   remove(): void;
 };
 
-export class BrowserStorage<SetConfig = unknown> {
+export type AsyncDefineResponse<SetConfig = unknown> = {
+  get<T = unknown>(): Promise<T | null>;
+  set(value: unknown, config?: SetConfig): Promise<boolean>;
+  remove(): Promise<void>;
+};
+
+export abstract class AbstractBrowserStorage<SetConfig = unknown> {
+  abstract adapter: Adapter<SetConfig> | AsyncAdapter<SetConfig>;
+  abstract prefix: string;
+  abstract serializer: Serializer;
+
+  protected toStore(value: unknown): string {
+    switch (typeof value) {
+      case "string":
+        return value;
+      case "undefined":
+        return this.serializer.stringify(null);
+      default:
+        return this.serializer.stringify(value);
+    }
+  }
+
+  protected fromStore<T = unknown>(item: unknown): T | null {
+    if (item === "null") return null;
+    if (typeof item !== "string") return null;
+
+    try {
+      return this.serializer.parse(item);
+    } catch (e) {
+    }
+
+    return (item as T) ?? null;
+  }
+}
+
+export class BrowserStorage<SetConfig = unknown> extends AbstractBrowserStorage<SetConfig> {
   readonly adapter: Adapter<SetConfig>;
   readonly prefix: string;
   readonly serializer: Serializer;
 
   constructor(config: StorageConfig = {}) {
+    super();
     this.adapter = config.adapter ?? new MemoryStorageAdapter();
     this.prefix = config.prefix ?? "";
     this.serializer = config.serializer ?? JSON;
@@ -78,28 +128,58 @@ export class BrowserStorage<SetConfig = unknown> {
       remove: () => this.remove(key),
     };
   }
+}
 
-  private toStore(value: unknown): string {
-    switch (typeof value) {
-      case "string":
-        return value;
-      case "undefined":
-        return this.serializer.stringify(null);
-      default:
-        return this.serializer.stringify(value);
-    }
+export class AsyncBrowserStorage<SetConfig = unknown> extends AbstractBrowserStorage<SetConfig> {
+  readonly adapter: AsyncAdapter<SetConfig>;
+  readonly prefix: string;
+  readonly serializer: Serializer;
+
+  constructor(config: StorageConfig & { adapter: AsyncAdapter<SetConfig> }) {
+    super();
+    this.adapter = config.adapter;
+    this.prefix = config.prefix ?? "";
+    this.serializer = config.serializer ?? JSON;
   }
 
-  private fromStore<T = unknown>(item: unknown): T | null {
-    if (item === "null") return null;
-    if (typeof item !== "string") return null;
+  clear() {
+    this.adapter.clear?.();
+  }
 
+  async get<T>(key: string): Promise<T | null> {
+    return this.fromStore<T>(await this.adapter.getItem(this.prefix + key));
+  }
+
+  async set(key: string, value?: unknown, config?: SetConfig): Promise<boolean> {
     try {
-      return this.serializer.parse(item);
-    } catch (e) {
+      await this.adapter.setItem(this.prefix + key, this.toStore(value), config);
+      return true;
+    } catch {
     }
+    return false;
+  }
 
-    return (item as T) ?? null;
+  async remove(key: string): Promise<void> {
+    await this.adapter.removeItem(this.prefix + key);
+  }
+
+  defineGroup<GenericRecord extends Record<string, string>>(
+    group: GenericRecord,
+  ): Record<keyof GenericRecord, AsyncDefineResponse<SetConfig>> {
+    return Object
+      .keys(group)
+      .reduce((prev, next) => ({
+        ...prev,
+        [next]: this.define<string>(group[next]),
+      }), {} as Record<keyof GenericRecord, AsyncDefineResponse<SetConfig>>);
+  }
+
+  define<DefinedType>(key: string, defaultConfig?: SetConfig): AsyncDefineResponse<SetConfig> {
+    return {
+      get: <T = DefinedType>(): Promise<T | null> => this.get<T>(key),
+      set: (value: unknown, config?: SetConfig) => this.set(key, value, config ?? defaultConfig),
+      remove: () => this.remove(key),
+    };
   }
 }
 
